9d30557cd8293e54775c00cdd39fa4b1d3c6ac59
Zemnmez <zemnmez@googlemail.com>
2014-08-01 23:41:48 +0100

    whoops, missed forward!

--- yesman.go
+++ yesman.go
@@ -213,5 +213,7 @@ func (s Server) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
 		s.Openid.ServeHTTP(rw, rq)
 	case "/login":
 		s.Login.ServeHTTP(rw, rq)
+	case "/forward":
+		s.Login.ServeHTTP(rw, rq)
 	}
 }
